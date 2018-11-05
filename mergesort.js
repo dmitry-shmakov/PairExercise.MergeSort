d0f22a00b39b05a3a49b62cc5be0b49520c35772
Melanie Hublard Hershman <melhhersh@gmail.com>
2018-11-05 15:27:52 -0500

    merge sort

--- mergesort.js
+++ mergesort.js
@@ -1,7 +1,49 @@
 function split(wholeArray) {
     /* your code here to define the firstHalf and secondHalf arrays */
     let midPoint = Math.floor(wholeArray.length/2);
-    let firstHalf = wholeArray.slice(0,midPoint+1);
-    let secondHalf = wholeArray.slice(midPoint+1)
+    let firstHalf = wholeArray.slice(0,midPoint);
+    let secondHalf = wholeArray.slice(midPoint)
     return [firstHalf, secondHalf];
+  }
+
+  function merge(array){
+    let firstNum = array[0][0];
+    let secondNum = array [1][0];
+    let resultArr = [];
+
+    while(0 < array[0].length || 0 < array[1].length){
+    if(firstNum < secondNum){
+      resultArr.push(firstNum);
+      array[0].shift();
+      firstNum = array[0][0]
+    }
+    else{
+      resultArr.push(secondNum);
+      array[1].shift();
+      secondNum = array[1][0]
+    }
+  }
+    return resultArr;
+  }
+
+  function mergeSort(array) {
+    let trigger = true;
+    let split1 = [];
+    let split2 = [];
+    let splitArr = split(array);
+
+    while (splitArr[0].length > 1 && splitArr[1].length >= 1){
+
+      if(splitArr[1] > 1){
+        split2 =mergeSort(splitArr[1]);
+      }
+      else{      
+        split2 = splitArr[1];
+      }
+      split1 = mergeSort(splitArr[0]);
+ 
+    }
+    return split1.concat(split2);
+    
+  
   }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,5 +1,15 @@
 describe('Split Array function', function() {
     it('is able to split an array into two halves', function() {
-      expect(split([1,2,3,4,5]).toEqual([[1,2,3], [4,5]])) 
+      expect(split([1,2,3,4,5])).toEqual([[1,2], [3, 4,5]])
     });
   });
+describe('Merge function', function(){
+        it('is able to merge two sorted arrays into one sorted array', function(){
+          expect(merge([[1,3],[2,4]])).toEqual([1,2,3,4])
+        });
+      });
+describe('MergeSort function', function(){
+        it('is able to split an array into two halves and merge the sorted arrays into one sorted array', function(){
+          expect(mergeSort([7, 8, 10, 3, 50, 9, 2])).toEqual([2, 3, 7, 8, 9, 10, 50])
+        });
+      });
