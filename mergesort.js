0065cd8a65f327989cc6032aa8495be32396b356
dmitry-shmakov <shmakov.dim@gmail.com>
2018-11-05 16:29:08 -0500

    Super close

--- mergesort.js
+++ mergesort.js
@@ -11,39 +11,49 @@ function split(wholeArray) {
     let secondNum = array [1][0];
     let resultArr = [];
 
-    while(0 < array[0].length || 0 < array[1].length){
-    if(firstNum < secondNum){
-      resultArr.push(firstNum);
-      array[0].shift();
-      firstNum = array[0][0]
+    while(array[0].length > 0 || array[1].length > 0){
+      array[0].length
+      if (array[0].length === 0 && array[1].length !== 0) {
+        for (let idx = 0; idx < array[1].length; idx++) {
+          resultArr.push(array[1][idx]);
+        }
+          array[1] = [];
+      } else if (array[0].length !== 0 && array[1].length === 0) {
+        for (let idx = 0; idx < array[0].length; idx++) {
+          resultArr.push(array[0][idx]);
+        }
+        array[0] = [];
+      } else {
+        if(firstNum < secondNum){
+          resultArr.push(firstNum); 
+          array[0].shift();
+          firstNum = array[0][0]
+        }
+        else {
+          resultArr.push(secondNum);
+          array[1].shift();
+          secondNum = array[1][0]
+        }
+      }
     }
-    else{
-      resultArr.push(secondNum);
-      array[1].shift();
-      secondNum = array[1][0]
-    }
-  }
     return resultArr;
   }
 
   function mergeSort(array) {
-    let trigger = true;
-    let split1 = [];
-    let split2 = [];
     let splitArr = split(array);
+    let split1 = splitArr[0];
+    let split2 = splitArr[1];
 
-    while (splitArr[0].length > 1 && splitArr[1].length >= 1){
-
-      if(splitArr[1] > 1){
-        split2 =mergeSort(splitArr[1]);
-      }
-      else{      
-        split2 = splitArr[1];
-      }
-      split1 = mergeSort(splitArr[0]);
- 
+    if (split1.length === 1 && split2.length === 1) {
+      // return [split1, split2];
+      return merge([split1, split2]);
     }
-    return split1.concat(split2);
-    
-  
+
+    if (split1.length === 1 && split2.length > 1) {
+      return merge([split1, mergeSort(split2)]);
+    }
+
+    split1 = mergeSort(split1);
+    split2 = mergeSort(split2);
+    return merge([split1, split2]);
   }
